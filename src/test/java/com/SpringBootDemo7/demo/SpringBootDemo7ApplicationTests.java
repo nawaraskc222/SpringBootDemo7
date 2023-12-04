7a6252169705447fc8833c2840f3e655e9ce763c
Nawaras kc <nawaraskc888@gmail.com>
2023-12-04 10:54:55 +0545

    first commit

---
+++ src/test/java/com/SpringBootDemo7/demo/SpringBootDemo7ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SpringBootDemo7.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDemo7ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
